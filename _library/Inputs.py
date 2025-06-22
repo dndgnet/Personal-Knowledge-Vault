2a98baec0f81cc939363036b71aa4112062da2f9
David Gordon <david@dndg.net>
2025-06-22 14:59:12 -0700

    replace spaces in tags with underscores

--- _library/Inputs.py
+++ _library/Inputs.py
@@ -7,6 +7,7 @@ from . import Tools as myTools
 from . import Terminal as myTerminal
 
 import os
+import re
 from datetime import datetime
 
 # Define the template and output paths
@@ -56,8 +57,9 @@ def get_project_name() -> tuple[dict,str, int]:
         exit(1)
 
     if int(selectedProject) == 0:
-        print(f"{myTerminal.ERROR}Not yet implemented, I haven't decided what to do with no project calls. Exiting.{myTerminal.RESET}")
-        exit(1)
+        # print(f"{myTerminal.ERROR}Not yet implemented, I haven't decided what to do with no project calls. Exiting.{myTerminal.RESET}")
+        # exit(1)
+        return projects, "", 0
 
     if int(selectedProject) == 1:
         newProjectName = input(f"{myTerminal.INPUTPROMPT}Enter a new project name: {myTerminal.RESET}").strip()
@@ -95,3 +97,42 @@ def get_template(templateType = "All") -> tuple[dict,str, int]:
         exit(1)
 
     return templates, templates[int(selectedTemplate)], int(selectedTemplate)
+
+def get_templateMerge_Values_From_User(timestamp_id,timestamp_date,timestamp_full,selectedProjectName,note_Content: str) -> str:
+    """
+    Get the template merge values from the user.
+    Returns the populated note content with user inputs replacing template tags.
+    """
+    
+    note_Content = note_Content.strip()
+    templateTags = re.findall(r"\[(.*?)\]", note_Content)
+    templateTags = set(templateTags)  # remove duplicates
+    for templateTag in templateTags:
+        templateTagValue = ""
+        if "[" in templateTag:
+            pass  # skip malformed tags and images
+        if templateTag.strip() == "":
+            pass
+        elif templateTag.upper() == "YYYYMMDDHHMMSS":
+            templateTagValue = timestamp_id
+        elif templateTag.upper() == "YYYY-MM-DD HH:MM:SS":
+            templateTagValue = timestamp_full
+        elif templateTag.upper() in ("YYYY-MM-DD","DATE"):
+            templateTagValue = timestamp_date
+        elif templateTag in ("Project Name","ProjectName","Project"):
+            templateTagValue = selectedProjectName
+        elif templateTag in ("Current User","User","Username"):
+            templateTagValue = os.getlogin()
+        elif templateTag in ["tags","Tags","TAGS"]:
+            templateTagValue = input(f"{myTerminal.INPUTPROMPT}Enter tags (comma-separated) or leave blank for none: {myTerminal.RESET}").strip()
+            tags = ""
+            for tag in templateTagValue.split(","):
+                tag = tag.strip().replace(" ","_")
+                tags += f"#{tag} "
+            templateTagValue = tags
+        else:
+            templateTagValue = input(f"{myTerminal.INPUTPROMPT}Enter value for [{templateTag}]: {myTerminal.RESET}").strip()
+
+        note_Content = note_Content.replace(f"[{templateTag}]", templateTagValue)
+        
+    return note_Content
