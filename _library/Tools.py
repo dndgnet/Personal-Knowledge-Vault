a91beaf4e11e982852438c5d839a7b38ac570a71
David Gordon <david@dndg.net>
2025-06-22 18:54:22 -0700

    add tools to extract formatter, tags, note title, and note date

--- _library/Tools.py
+++ _library/Tools.py
@@ -104,6 +104,9 @@ def get_note_tags(noteFileNameAndPath: str) -> tuple [str,set]:
     
     filename =  noteFileNameAndPath.split("/")[-1]
     
+    if not filename.endswith(".md"):
+        return filename, set()  # return empty set if not a markdown file
+    
     # Read the template content
     with open(noteFileNameAndPath, 'r', encoding='utf-8') as f:
         note  = f.read()
@@ -124,7 +127,6 @@ def get_note_tags(noteFileNameAndPath: str) -> tuple [str,set]:
         tag = tag.strip()
         if tag.startswith("#"):
             tag = tag[1:]  # Remove the leading hash if present
-        
         allTags.add(tag)
 
     return filename, allTags
@@ -155,3 +157,211 @@ def get_project_tags(projectName: str) -> dict:
                 tags[tag] = existing_tags
     
     return tags
+
+def get_NoteFiles_dict(target_dir: str) -> dict:
+    files_dict = {}
+    for root, dirs, files in os.walk(target_dir, topdown=True):
+        for file in files:
+            if not file.startswith('.'):  # Skip hidden files
+                uniqueIdentifier = file.split(".")[0]
+                if file.endswith('.md'):
+                    files_dict[uniqueIdentifier] = [file, os.path.join(root, file)]
+                    
+    return files_dict
+
+def get_NoteFiles_withTags_dict(target_dir: str) -> dict:
+    files_dict = {}
+    for root, dirs, files in os.walk(target_dir, topdown=True):
+        for file in files:
+            if not file.startswith('.'):  # Skip hidden files
+                uniqueIdentifier = file.split(".")[0]
+                if file.endswith('.md'):
+                    files_dict[uniqueIdentifier] = [file, os.path.join(root, file), get_note_tags(os.path.join(root, file))]
+                    
+    return files_dict
+
+def get_pkv_NoteFiles() -> dict:
+    """
+    Returns a dictionary of all files in the PKV.
+    
+    Returns:
+        dict: A dictionary where keys are filenames and values are their full paths.
+    """
+    
+    pkvPath = myPreferences.root_pkv()
+    if not os.path.isdir(pkvPath):
+        return {}
+    
+    files_dict = get_NoteFiles_dict(pkvPath)
+    
+    return files_dict
+
+def get_project_NoteFiles(projectName: str) -> dict:
+    """
+    Returns a dictionary of all files in a specific project.
+    
+    Args:
+        projectName (str): The name of the project.
+        
+    Returns:
+        dict: A dictionary where keys are filenames and values are their full paths.
+    """
+    
+    projectPath = os.path.join(myPreferences.root_projects(), projectName)
+    if not os.path.isdir(projectPath):
+        return {}
+    
+    files_dict = get_NoteFiles_dict(projectPath)
+    
+    return files_dict
+
+def is_NewNote_identifier_unique(noteIdentifier) -> bool:
+    """
+    Checks if a note identifier is unique across all notes in the PKV.
+    
+    Args:
+        noteIdentifier (str): The identifier to check for uniqueness.
+        
+    Returns:
+        bool: True if the identifier is unique, False otherwise.
+    """
+    pkvFiles = get_pkv_NoteFiles()
+    
+    if noteIdentifier in pkvFiles.keys():
+        return False
+    else: 
+        return True
+
+def get_note_frontMatter(noteBody: str) -> str:
+    """
+    Extracts the front matter from a note body.
+    
+    Args:
+        noteBody (str): The content of the note.
+        
+    Returns:
+        str: The front matter of the note, if present; otherwise, an empty string.
+    """
+    
+    frontMatter_match = re.search(r'^---\n(.*?)\n---', noteBody, re.DOTALL)
+    if frontMatter_match:
+        return frontMatter_match.group(1).strip()
+    
+    return ""
+
+def get_note_title_from_frontMatter(frontMatter: str) -> str:
+    """
+    Extracts the title from the front matter of a note.
+    
+    Args:
+        frontMatter (str): The front matter of the note.
+        
+    Returns:
+        str: The title of the note, if present; otherwise, an empty string.
+    """
+    
+    title_match = re.search(r'title:\s*(.*)', frontMatter)
+    if title_match:
+        return title_match.group(1).strip()
+    
+    return ""
+
+def get_note_date_from_frontMatter(frontMatter: str) -> str:
+    """
+    Extracts the date from the front matter of a note.
+    
+    Args:
+        frontMatter (str): The front matter of the note.
+        
+    Returns:
+        str: The date of the note, if present; otherwise, an empty string.
+    """
+    
+    date_match = re.search(r'date:\s*(.*)', frontMatter.replace("*",""))
+    if date_match:
+        return date_match.group(1).strip()
+    
+    date_match = re.search(r'created:\s*(.*)', frontMatter)
+    if date_match:
+        return date_match.group(1).strip()
+    
+    return ""
+
+def get_pkv_tags() -> dict:
+    """
+    Extracts tags from all notes in the PKV.
+        
+    Returns:
+        dict: A dictionary where keys are note filenames and values are sets of tags found in those notes.
+    """
+    
+    pkvPath = os.path.join(myPreferences.root_projects())
+    
+    tags = {}
+    for filename in sorted(os.listdir(pkvPath)):
+        noteFilePath = os.path.join(pkvPath, filename)
+        if os.path.isfile(noteFilePath) and filename.endswith(".md"):
+            _, noteTags = get_note_tags(noteFilePath)
+            for tag in noteTags:
+                existing_tags = tags.get(tag, set())
+                existing_tags.add(filename)
+                tags[tag] = existing_tags
+    
+    return tags
+
+def get_Note_with_TODO(target_dir: str) -> dict:
+    """
+    Returns a dictionary of notes that contain TODO items.
+    value dictionary contains keys "notePathAndFile" and "frontMatter".
+    """
+    files_dict = {}
+    for root, dirs, files in os.walk(target_dir, topdown=True):
+        for file in files:
+            if not file.startswith('.') and file.endswith(".md"):  # skip hidden files
+                notePathAndFile = os.path.join(root, file)
+                with open(notePathAndFile, 'r', encoding='utf-8') as f:
+                    noteBody  = f.read()
+                    if '#TODO' in noteBody:  
+                        uniqueIdentifier = file.split(".")[0]
+                        files_dict[uniqueIdentifier] = {"notePathAndFile": notePathAndFile,
+                                                        "frontMatter": get_note_frontMatter(noteBody)}
+                    
+    return files_dict
+
+def get_Note_with_INCOMPLETE(target_dir: str) -> dict:
+    """
+    Returns a dictionary of notes that contain INCOMPLETE items.
+    value dictionary contains keys "notePathAndFile" and "frontMatter".
+    """
+    files_dict = {}
+    for root, dirs, files in os.walk(target_dir, topdown=True):
+        for file in files:
+            if not file.startswith('.') and file.endswith(".md"):  # skip hidden files
+                notePathAndFile = os.path.join(root, file)
+                with open(notePathAndFile, 'r', encoding='utf-8') as f:
+                    noteBody  = f.read()
+                    if '#INCOMPLETE' in noteBody:  
+                        uniqueIdentifier = file.split(".")[0]
+                        files_dict[uniqueIdentifier] = {"notePathAndFile": notePathAndFile,
+                                                        "frontMatter": get_note_frontMatter(noteBody)}
+                    
+    return files_dict
+
+def get_Note_with_ActionItems(target_dir: str) -> dict:
+    """
+    Returns a dictionary of notes that contain Action items.
+    value dictionary contains keys "notePathAndFile" and "frontMatter".
+    """
+    files_dict = {}
+    for root, dirs, files in os.walk(target_dir, topdown=True):
+        for file in files:
+            if not file.startswith('.') and file.endswith(".md"):  # skip hidden files
+                notePathAndFile = os.path.join(root, file)
+                with open(notePathAndFile, 'r', encoding='utf-8') as f:
+                    noteBody  = f.read()
+                    if "[ ]" in noteBody:  
+                        uniqueIdentifier = file.split(".")[0]
+                        files_dict[uniqueIdentifier] = {"notePathAndFile": notePathAndFile,
+                                                        "frontMatter": get_note_frontMatter(noteBody)}
+                    
+    return files_dict
