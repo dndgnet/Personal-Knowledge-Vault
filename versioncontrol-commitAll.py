2266e60b5b54d1128c8616fe66fabe156c6c301f
David Gordon <david@dndg.net>
2025-11-22 16:43:59 -0800

    add a prompt for the commit all version control command

--- versioncontrol-commitAll.py
+++ versioncontrol-commitAll.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from _library import VersionControl as myVersionControl
- 
-myVersionControl.add_and_commit_all()
+
+commit_message = input("Enter commit message for committing all changes (leave blank for default): ").strip()
+myVersionControl.add_and_commit_all(commit_message)
 
