cd888a5b81089f892025100372d6416021a55efa
David Gordon <david@dndg.net>
2026-01-03 10:34:08 -0800

    fix get action item behavior, command was grouping by project then looping through the projects.  This created a scenario where select by number worked some times but failed when projects are not in perfect alphabetic order. #30

--- get-actionItems.py
+++ get-actionItems.py
@@ -9,26 +9,26 @@ if not todoNotes:
     print(f"{myTerminal.GREEN}No Action items found.{myTerminal.RESET}")
     exit(0)
 
+#sort notes by project name 
+todoNotes.sort(key=lambda x: (x.project.lower(), x.date))
+
 index = 0    
 
 print(f"{myTerminal.SUCCESS} {len(todoNotes)} Action items found:{myTerminal.RESET}")
 
-projects = [note.project for note in todoNotes]
-projects = list(set(projects))
-projects.sort()
-
-for project in projects:
-    if project == "":
-        print(f"{myTerminal.BLUE}no project{myTerminal.RESET}")
-    else:
-        print(f"{myTerminal.BLUE}Project: {project}{myTerminal.RESET}")
-    projectNotes = [note for note in todoNotes if note.project == project]
-
-    for note in projectNotes:
-        index += 1
-        print(f"\t{myTerminal.WHITE}{index:>2}. - {note.title[:40]:<40} ({note.date}){myTerminal.RESET}")
-        for actionItem in note.actionItems: 
-            myTerminal.printWithoutLineWrap(prefixText=f"\t\t{myTerminal.GREY} - [ ] ", textToAdd=f"{actionItem}{myTerminal.RESET}")
+project = None
+for note in todoNotes:
+    if note.project != project:
+        project = note.project
+        if project == "":
+            print(f"{myTerminal.BLUE}no project{myTerminal.RESET}")
+        else:
+            print(f"{myTerminal.BLUE}Project: {project}{myTerminal.RESET}")
+    
+    index += 1
+    print(f"\t{myTerminal.WHITE}{index:>2}. - {note.title[:40]:<40} ({note.date}){myTerminal.RESET}")
+    for actionItem in note.actionItems: 
+        myTerminal.printWithoutLineWrap(prefixText=f"\t\t{myTerminal.GREY} - [ ] ", textToAdd=f"{actionItem}{myTerminal.RESET}")
 
 selected = input(f"{myTerminal.WHITE}Select note item by number (1-{index}) or press Enter to exit: {myTerminal.RESET}")
 
